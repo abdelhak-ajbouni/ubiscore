7c10c6c4a6b42fd1de8ca4a82d75fea360ba3166
abdelhak.ajbouni <abdelhak.ajbouni@gmail.com>
2022-04-06 15:15:39 +0100

    init project

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Login</title>
+        <meta name="description" content="Login page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Login</h1>
+      </main>
+
+    </div>
+  )
+}
+
+export default Home
